298d22a9f5c6b0b51a183f79e3bd36dff772e365
codebynishan <mail.nishanrana@gmail.com>
2025-07-03 22:28:03 +0545

    Chore:learning set and practise question

---
+++ python/day_6.py
@@ -0,0 +1,75 @@
+# # set
+# b = set({})
+# a = {"sudan","dang",1,4}
+# print(type(a))
+# print(a)
+
+# ADD SET item
+# a ={"Nishan","Rana",1,2,3}
+# print(a)
+# (a.add("school"))
+# print("after add",a)
+
+# update ()
+# a ={"Nishan","Rana",1,2,3}
+# b =[2,3,4,5,]
+# a.update(b)
+# print (a)
+
+# Remove()
+# a ={"Nishan","Rana",1,2,3}
+# a.remove("Rana")
+# print(a)
+
+# clear(). -- this will empty set
+# a ={"Nishan","Rana",1,2,3}
+# a.clear()
+# print(a)
+
+# Merge two dictionaries:
+# Which value should y hold?
+# d1 = {'x': 1, 'y': 2} 
+# d2 = {'y': 3, 'z': 4}
+# d1.update(d2)
+# print(d1)
+# print(d1.get('y'))
+
+# Convert a list with duplicate elements into a unique set and then back to a sorted list
+# num ={1,1,2,3,4,5,5,6,2,1,4}
+# print(num)
+
+# Write a condition using is not to check if a variable is not None
+# string = "Hello"
+# if string is not None:
+#     print("The variable has a value:", string)
+# else:
+#     print("The variable is None")
+
+# Use an if-elif-else to categorize temperature as Cold (<15), Warm (15–25), or Hot (>25).    
+# temp = int(input("enter temperature:"))
+# if temp<15:
+#     print("cold")
+# elif temp>25:
+#     print("hot")
+# elif temp>15 and temp<=25:
+#     print("warm")
+
+# Write a nested if statement to check if a number is divisible by both 3 and 5.
+# number = int(input("enter any nunber:"))
+# if number%5==0:
+#     if number%3==0:
+#         print(f'{number} is divisible by 5 and 3')
+# else:
+#     print(f'{number} is not divisible by 3 and 5')
+    
+
+
+# Check if a student (e.g., "Alice") is in the students list.
+# students = ["Alice", "Bob", "Charlie", "David", "Eve"]
+# if  "Alice" in students:
+#     print("alice is in student list ")
+# else:
+#     print("alice is not in list")
+    
+
+    
