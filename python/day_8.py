1a030e6192f6904ac72dec3d99e9e3f02f7934d7
codebynishan <mail.nishanrana@gmail.com>
2025-07-09 15:24:20 +0545

    feat:while loop

---
+++ python/day_8.py
@@ -0,0 +1,100 @@
+# while loop
+
+# i = 6
+# while i<6:
+#     print(i)
+#     i += 1
+
+# i = 1
+# while i<6:
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
+
+# i = 0
+# while i< 5:
+#     print(i)
+#     i +=1
+
+# i = 10
+# while i>0:
+#     print(i)
+#     i -= 1
+    
+# total = 0
+# while True:
+#     num = int(input("enter a number (0 to stop):"))
+#     if num ==0:
+#         break
+#     total += num
+# print("sum:",total)
+
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+# else:
+#   print("i is no longer less than 6")
+
+
+
+# while(i<=5):
+#     print("hrllo")
+#     break
+# i = 6
+# while loop 
+# i = 1
+# while i <=20:
+#     print (i)
+#     i+=1
+#     if i == 11:
+#         break
+    
+# while True:
+#     num = input("Enter a positive integer: ")
+    
+   
+#     if num.isdigit() and int(num) > 0:
+#         break  
+#     else:
+#         print("Invalid input. Please enter a positive integer.")
+
+
+# input = int(input("enter any number:"))
+# sum = 0
+# i = 1
+# while i <= input:
+#     sum += i
+#     i += 1
+# print(sum)
+    
+# nested while loop
+# list1 =[1,2,3]
+# list2=[4,5,6]
+# i = 0
+# j =0
+# while i<len(list1):
+#     while j < len (list2):
+#         print(list1[i],list2[j])
+#         j +=1
+#     print()
+#     i += 1
+
+# import random
+# conn = random.randint(1,10)
+# print(conn)
+# count =1
+# max_count = 3
+# while (count<=max_count):
+#     userinput = int(input("enter a number: "))
+#     count += 1
+#     if conn == userinput:
+#         print("number match")
+#         break
+#     else:
+#         print("please try again")
+# if count == max_count and conn != userinput:
+#     print("you are on max count")
+
