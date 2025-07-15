a0ee79d3f74a146b4d6173d507e6944082240d7c
codebynishan <mail.nishanrana@gmail.com>
2025-07-15 23:21:54 +0545

    feat:added some examples

--- python/day_11.py
+++ python/day_11.py
@@ -86,8 +86,44 @@
 # print(obj.add())
 # print(obj.nul())
 
+# class student:
+#     def __init__(self,name,marks):
+#         self.name = name
+#         self.marks = marks
+        
+#     def get_avg(self):
+#             sum = 0
+#             for val in self.marks:
+#                 sum += val
+#             print("hi",self.name,"your avg score is :",sum/3)
+            
+# s1 = student("Nishan rana",[99,78,90])
+# s1.get_avg()
+# s1.name = "sagar"
+# s1.get_avg
 
 
+# class account:
+#     def __init__(self,bal,acc):
+#         self.balance = bal
+#         self.account_no = acc
+     
+#     def debit(self,amount):
+#         self.balance -= amount
+#         print("Rs",amount,"was debited")
+#         print("total balance =",self.get_balance())
+        
+#     def credit(self,amount):
+#         self.balance += amount
+#         print("Rs",amount,"was credited")
+#         print("total balance =",self.get_balance())
+    
+#     def get_balance(self):
+#         return self.balance
+                   
+# acc1 = account(10000,12345)
+# acc1.debit(1000)
+# acc1.credit(500)
 
 
 
