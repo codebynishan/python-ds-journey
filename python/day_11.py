31356136431f4c5bd2e61512bae7b35dd013be06
codebynishan <mail.nishanrana@gmail.com>
2025-07-15 22:34:03 +0545

    feat:python class

---
+++ python/day_11.py
@@ -0,0 +1,95 @@
+# python class
+
+# class test:
+#     x = 5
+# obj = test()
+# print(obj.x)
+
+
+    
+# class person:
+#     name = "Nishan"
+#     age = 23
+#     occ = "student"
+#     def info(self):
+#         print(f"{self.name} is {self.occ}")
+# obj = person()
+# obj1 = person()
+# obj.name ="sagar"
+# obj1.name ="Binod"
+# print(obj.name)
+# obj.info()
+# obj1.info()
+
+
+# class person:
+#     def __init__(self,name,age):
+#         print("this is init ")
+#         self.name = name
+#         self.age = age
+#     def info(self):
+#         print(f'{self.name} is {self.age}years old')
+# obj = person("Nishan",23)
+# obj.info()
+
+# class student:
+#     def __init__(self):
+#         print("adding new student in database....")
+# obj = student()    
+
+# class test():
+#      a ="hello"
+# obj = test()
+# print(obj)
+
+# class math():
+#     a = 50
+#     b = 60
+    
+#     def __str__(self):
+#         return "this is str method"
+#     def add(self):
+#        return self.a +self.b
+    
+#     def mul(self):
+#         return self.add()*3
+    
+# obj = math()
+# print(obj)
+# print(obj.add())
+# print(obj.mul())
+    
+    
+# class test3():
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+#         print("i am init")
+#         return
+#     def test(self):
+#         print(self.num)
+
+
+# class math():
+#     a = 20
+#     b = 30
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+#         return 
+
+#     def add(self):
+#         return self.a + self.b
+#     def mul(self):
+#         return self.add()*3
+# obj = math()
+# print(obj.add())
+# print(obj.nul())
+
+
+
+
+
+
+    
+    
