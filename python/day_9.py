6dfe3db85279d6ac0f5650313e0edef9f33a2a82
codebynishan <mail.nishanrana@gmail.com>
2025-07-22 14:06:49 +0545

    feat:def function

---
+++ python/day_9.py
@@ -0,0 +1,114 @@
+# function
+
+# def hello():
+#     print("Hello")
+# hello()
+
+# def greet():
+#     return("Hello World")
+# print(greet())
+
+
+# def repeat_word(word):
+#     return word *3
+# print(repeat_word("sudeep"))
+
+# def make_upper(text):
+#     return text.upper()
+# print(make_upper("hello"))
+    
+
+# def cal_sum(a,b):
+#     sum = a + b
+#     print(sum)
+#     return sum
+# cal_sum(2,3)
+# cal_sum(6,9)
+
+# def square(num):
+#     return num *num
+# print(square(int(input("enter any number:"))))
+
+
+
+# def average(a,b,c):
+#     avg = (a+b+c)/3
+#     print(avg)
+#     return avg
+# average(4,5,6)
+
+# def get_data():
+#     f_name = "Nishan"
+#     l_name = "Rana"
+#     return "Nishan Rana"
+# print(get_data())
+
+# a = int(input("enter any number:"))
+# b = int(input("enter any number:"))
+# def calculateGmean(a, b):
+#     mean =(a*b)/(a+b)
+#     print(mean)
+# calculateGmean(a, b)
+
+
+
+# def pull_data(name,age):
+#     return f'My name is {name}.I am {age} years old.'
+# print(pull_data("Nishan",23))
+
+# user_input =int(input("enter any number:"))
+# def check():
+#     if user_input == 5:
+#         return "number match"
+#     return "number doesnot match"
+# print(check())
+
+# import random 
+# def guess_random_number(lastdata = 10):
+#     return  random.randint(1,lastdata)
+    
+    
+# print(guess_random_number(50))
+# print(guess_random_number())
+# print(guess_random_number())
+# print(guess_random_number(4))
+# print(guess_random_number())
+# print(guess_random_number(90))
+# print(guess_random_number())
+
+
+
+# import random
+# def get_user_choice():
+#     user_choice =input("enter your choice from(scissor/paper/rock):")
+#     return user_choice
+# def get_choice():
+#     computer =random.choice(['rock','scissor','paper'])
+#     return computer
+# # print(get_choice())
+
+# def game():
+#     user_input =get_user_choice()
+#     computer = get_choice()
+#     if user_input==computer:
+#         return"Tie"
+       
+#     elif (user_input == 'rock' and computer == 'scissors') or \
+#             (user_input == 'scissors' and computer == 'paper') or \
+#             (user_input == 'paper' and computer == 'rock'):
+#             return "You win!"
+#     else:
+#             return f"Computer wins! {computer}"
+    
+       
+# print(game())
+    
+
+
+
+
+
+
+
+
+
