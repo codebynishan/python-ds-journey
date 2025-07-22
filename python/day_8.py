8ecc21ba6aedc566746d7b172c8491dba835fa8e
codebynishan <mail.nishanrana@gmail.com>
2025-07-22 14:06:22 +0545

    feat:while loop and nested while loop

---
+++ python/day_8.py
@@ -0,0 +1,121 @@
+# while loop
+
+# i = 6
+# while i<6:
+#     print(i)
+#     i += 1
+
+# i = 1
+# while i<6:
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
+
+# i = 0
+# while i< 5:
+#     print(i)
+#     i +=1
+
+# i = 10
+# while i>0:
+#     print(i)
+#     i -= 1
+    
+# total = 0
+# while True:
+#     num = int(input("enter a number (0 to stop):"))
+#     if num ==0:
+#         break
+#     total += num
+# print("sum:",total)
+
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+# else:
+#   print("i is no longer less than 6")
+
+
+
+# while(i<=5):
+#     print("hrllo")
+#     break
+# i = 6
+# while loop 
+# i = 1
+# while i <=20:
+#     print (i)
+#     i+=1
+#     if i == 11:
+#         break
+    
+# while True:
+#     num = input("Enter a positive integer: ")
+    
+   
+#     if num.isdigit() and int(num) > 0:
+#         break  
+#     else:
+#         print("Invalid input. Please enter a positive integer.")
+
+
+# input = int(input("enter any number:"))
+# sum = 0
+# i = 1
+# while i <= input:
+#     sum += i
+#     i += 1
+# print(sum)
+ 
+# nested while loop
+# list1 =[1,2,3]
+# list2=[4,5,6]
+# i = 0
+# j =0
+# while i<len(list1):
+#     while j < len (list2):
+#         print(list1[i],list2[j])
+#         j +=1
+#     print()
+#     i += 1
+
+
+# import random
+# com = random.randint(1,10)
+# print(com)
+# user = 0
+# computer = 0
+# while(True):
+#     userinput = int(input("enter a number: "))
+
+#     if com == userinput:
+#         print("number match ")
+#         user = user + 10
+#         computer = computer-5
+#         com = random.randint(1,10)
+
+#     else:
+#         print("please try again")
+#         user = user - 5
+#         computer = computer+10
+
+#     if computer>=100 or user>=100:
+#         if computer>=100:
+#             print("computer win")
+#             break
+#         elif user>=100:
+#             print("user win")
+#             break
+
+# print(int(random.random()*100))
+
+
+# print(random.randint(1,10))
+# a = ["sudan","suman","hari","subash"]
+# print(random.choice(a))
+
+
+# com=random.randint(20,25)##print(com)user=int(input("enter the number from 20-24"))if com==user:    print("nice")else:    print("better luck next time")
