73269c5de4377123348b9901eec93a989550589d
codebynishan <mail.nishanrana@gmail.com>
2025-07-22 14:07:25 +0545

    feat:lambda function and recursion

---
+++ python/day_10.py
@@ -0,0 +1,61 @@
+# recursion
+
+
+
+# def test():
+#     print("i am function")
+#     test()
+#     return"hello"
+# test()
+
+# def fact(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return n * fact(n-1)
+    
+# print(fact(3))
+
+# a = [1,[[2]],3,[4,[[[5],[6],[[[7]]]]]]]
+# [1,2,3,4,5,6,7]
+
+# def flattern_list(n):
+#     for item in list:
+#         if isinstance
+
+
+# b=[]
+# def flatten_list(n):
+#     for val in n:
+#         if  isinstance(val,list):
+         
+#           flatten_list(val)
+#         else:
+#          b.append(val)
+#     return b
+
+
+# print(flatten_list(a))
+
+# def cal_sum(n):
+#     if(n==0):
+#         return
+#     print(n)
+#     cal_sum(n-1)
+# cal_sum(5)
+
+
+# lambda
+
+# x = lambda a,b:a+b
+# print(x(1,3))
+
+# x = lambda *args, **kwargs:args, kwargs
+# print(x(1,name ="hello"))
+# def double(x):
+#     return x*2
+# double = lambda x: x*2
+# print(double(5))
+
+# avg = lambda x,y,z :(x+y+z)/3
+# print(avg(3,5,10))
