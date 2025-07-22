5325ff28b60f31ef9c3a08fee822e99f286c3381
codebynishan <mail.nishanrana@gmail.com>
2025-07-22 13:58:32 +0545

    chore: Python Installation and Code

---
+++ python/day_1.py
@@ -0,0 +1,32 @@
+
+# print("hello world")
+# print(1 == "1")
+# print(10!=12)
+
+# logical operator(and or not)
+# print(2==7 and 1!=4)
+# print(1>2 or 1!=4)
+
+# input function
+# a = input()
+# print("thwe value of a is",a)
+
+# concade
+# print("nishan" *2)
+# print("class"+"class")
+
+# typecasting
+# a = int(input("enter any number"))
+# b = int(input("enter any number"))
+# print(a+b)
+# print("2"+"100")
+
+# string
+# a = "hello"
+# print(a.upper())
+# print(a.isdigit())
+# print(a.capitalize())
+# print(a.count())
+
+
+
