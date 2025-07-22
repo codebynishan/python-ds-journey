a3dc4bfb6408aeca1d5e29ba1e80e2dafac51d75
codebynishan <mail.nishanrana@gmail.com>
2025-07-22 14:01:48 +0545

    feat:python conditions

---
+++ python/day_2.py
@@ -0,0 +1,95 @@
+
+# if (2==2):
+#     print("hello")
+# if(2==2):
+#     print("this is if")    
+# elif(2!=4):
+#     print("i am from elif")
+# else:
+#     print("from else"
+
+# percentage =80
+# if (percentage >= 80):
+#     print("A")
+# elif(percentage>=70):
+#     print("B+")
+# elif(percentage >= 60):
+#     print("B")
+
+# n = int(input("enter any nuumber:"))
+# if (n%2==0):
+#     print(f"{n} number is even")
+# else:
+#     print(f"{n} number is odd")
+
+# Nested if
+# name ="Nshan"
+# if(2==2):
+#     if(name=="sudan"):
+#         print("hello i am Nishan")
+#     else:
+#         print("name does not match")
+#     print("this is if condition")
+# else:
+#     if(true):
+#         print("i am nested if condition")
+#     print("this is else condition")    
+
+# Single line if condition
+# n = int(input("enter any number:"))
+# a = "even"if n%2==0 else"odd"
+# print(a)
+
+# Identity operator(is,is not)
+# a = None
+# print(a is None)
+# print(a is not None)
+
+
+# write a program on employ bonus system
+# year_of_service =int(input("Enter year of service:"))
+# rating =float(input("Enter number between 0 to 5:"))
+# if year_of_service > 5 and rating > 4.5:
+#     print("Worker get 20% bonus")
+# elif year_of_service >3 and rating > 4:
+#     print("Worker get 10% bonus")
+# elif year_of_service >1 and rating >3.5:
+#     print("Worker get 5% bonus")
+# else:
+#     print("worker get no bonus")
+
+
+# Ask the user to input three sides of a triangle.
+# side_a = int(input("enter any number:"))
+# side_b = int(input("enter any number:"))
+# side_c = int(input("enter any number:"))
+# if side_a==side_b==side_c:
+#     print("Equilateral(all sides equal)")
+# elif side_a == side_b or side_b == side_c or side_a == side_c:
+#     print("Isosceles (two sides equal)")  
+# else:
+#     print("Scalene (all sides different)")
+
+
+# Simulate a traffic light system
+# import sys
+# light= input("enter any color from red,yellow,green:")
+
+# if light not in ('red', 'yellow', 'green'):
+#     print(f"{light} is an invalid option. Choose from red, yellow, green.")
+#     sys.exit()
+
+    
+# if light=='red':
+#     print("stop")
+# elif light =='yellow':
+#     print("slow down and prepare to stop")
+# elif light =='green':
+#     print("there is a predstain waiting, stop")
+# else:
+#     print("go")
+    
+    
+# check if a given password is strong
+# password = input("enter your password")
+# if password ==
