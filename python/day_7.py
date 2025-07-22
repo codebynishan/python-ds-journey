5a834e8e2e6c2d5d63047f218efc7bbb0ed45d7c
codebynishan <mail.nishanrana@gmail.com>
2025-07-22 14:05:54 +0545

    feat:for loop and nested for loop

---
+++ python/day_7.py
@@ -0,0 +1,139 @@
+# for loop
+
+# for i in [1,2,3,4,5,6]:
+#     print(i+1)
+
+# for i in ["sudan","suman","japan"]:
+#     print("my name is ", i)
+
+# for i in range(1,10,2):
+#     print(i)
+
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     break
+#   print(x)
+
+# for x in range(6):
+#     if x== 3:
+#         continue
+#     print(x)
+
+# for x in range(1,6,2):
+#     print(x)
+
+# for x in range (6):
+#     print(x)
+# else:
+#     print("finally finished")
+
+
+#  in break else statemnt doesnot run because it terminate the loop 
+# for i in range (10):
+#     if i == 5:
+#         break
+#     print(i)
+# else:
+#     print("finally finished")
+   
+#  in continue else statement run because it skip the  loop
+# for i in range (10):
+#     if i == 5:
+#         continue
+#     print(i)
+# else:
+#     print("finally finished")   
+
+
+
+# loop
+# n = input("enter a number: ")
+# n = "1"
+# if n.isdigit():
+#     n = int(n)
+#     for i in range(1,11,1):
+#         print(f'{n} X {i} = {n*i}')
+#         print()
+# else:
+#     print(f"enter number {n} is invalid number")
+
+
+# nested loop
+
+
+# first_name =["Nishan","suman","sudan"]
+# last_name = ["Rana","gaha","bhandari"]
+
+# for i in first_name:
+#     for x in last_name:
+#        print(i,x)
+
+# names = ["alice","bob","charlie","david"]
+# for i in names:
+#     print(i)       
+       
+# for i in range(0,51,2):
+#     print(i)    
+
+# for i in range(3):
+#     for j in range(3):
+#         print("*",end=" ")
+#     print()
+    
+
+# for i in range(1, 6):  
+#     for j in range(i):
+#         i +=1
+#         print(j, end=" ")
+#     print()  
+
+
+# a = [1,2,32,3,5,6,7]
+
+# for i in a:
+#     if i == 32:
+#         break
+#     print(i)
+
+
+# a = [1,2,32,3,5,6,7]
+# for i in a:
+#     if i == 32:
+#         continue
+#     print(i)
+
+
+# range(50,60,1)
+
+
+# nested loop
+# for i in range(1,4):
+#     for j in range(5,7):
+#         print(i,j)
+
+# for i in range(3):
+#     for x in range(1,10):
+#         print(x,end=" ")
+#     print()
+
+# list comprehension
+# a = []
+# for i in range(1,20):
+#     a.append(i)
+
+# print(a)
+
+# a= [ i for i in range(1,20)]
+# print(a)
+
+# a = [1,2,3]
+# sum = 0
+# for i in a:
+#     sum = sum + i
+    # sum+=i
+
+# print(sum)
+
+        
