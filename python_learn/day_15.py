5ec9810a06e6e5b2206b0f654e810d01457677e6
codebynishan <mail.nishanrana@gmail.com>
2025-07-27 21:51:40 +0545

    feat:connector

--- python_learn/day_15.py
+++ python_learn/day_15.py
@@ -13,4 +13,20 @@ if mydb.is_connected:
 else:
     print("error....")
     
-
+mycursor = mydb.cursor()
+# mycursor.execute("show tables")
+# mycursor.execute("show columns from student")
+insert ="""insert into student(id,full_name,age,address,faculty) 
+values(%s,%s,%s,%s,%s)"""
+data =[(1,"Nishan Rana",23,"ktm","science"),
+       (2,"sagar thapa",21,"lalitpur","management"),
+       (3,"hero",24,"bhaktpur","science"),
+       ]
+mycursor.executemany(insert,data)
+mydb.commit()
+mycursor.execute("select * from studdent")
+result = mycursor.fetchall()
+print(result)
+# print(f'{mycursor.rowcount} rows inserted')
+mycursor.close()
+mydb.close()
