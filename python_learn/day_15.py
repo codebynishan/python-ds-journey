3fff047d6a7f702af443e5404ad6ba65b8be6e0f
codebynishan <mail.nishanrana@gmail.com>
2025-07-28 20:02:26 +0545

    feat:python with mysql connector

--- python_learn/day_15.py
+++ python_learn/day_15.py
@@ -1,12 +1,16 @@
 # Run this in Python shell
 import mysql.connector
 print(mysql.connector.__version__) 
+from tabulate import tabulate
 
 mydb = mysql.connector.connect(
     host ="localhost",
     user ="root",
-    port =3307,
+    port =3306,
+    password ="Nishan@2059",
     database ="school"
+    
+    
 )
 if mydb.is_connected:
     print("connection successfull....")
@@ -14,19 +18,24 @@ else:
     print("error....")
     
 mycursor = mydb.cursor()
-# mycursor.execute("show tables")
+
 # mycursor.execute("show columns from student")
-insert ="""insert into student(id,full_name,age,address,faculty) 
-values(%s,%s,%s,%s,%s)"""
-data =[(1,"Nishan Rana",23,"ktm","science"),
-       (2,"sagar thapa",21,"lalitpur","management"),
-       (3,"hero",24,"bhaktpur","science"),
-       ]
+
+# for column in mycursor.fetchall():
+#     print(column)
+
+
+insert ="insert into student(id,name,age,address,faculty)values(%s,%s,%s,%s,%s)"
+data = [(11,'nishan rana',23,'ktm','science'),
+        (22,'sagar thapa',20,'lalitpur','management')]
 mycursor.executemany(insert,data)
 mydb.commit()
-mycursor.execute("select * from studdent")
+
+mycursor.execute("select * from student")
 result = mycursor.fetchall()
+
+header = "id","name","age","address","faculty"
+print(tabulate(result,headers=header,tablefmt="grid"))
 print(result)
-# print(f'{mycursor.rowcount} rows inserted')
-mycursor.close()
-mydb.close()
+
+
